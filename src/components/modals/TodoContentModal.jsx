f4bcec1c2a55e6756446d225424d02062523245c
Muneer Hashmat <muneerhashmat@gmail.com>
2025-05-23 12:21:39 +0530

    create todo modal for adding and editing todo content

---
+++ src/components/modals/TodoContentModal.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const TodoContentModal = () => {
+  return (
+    <div className="modal-container">
+      <div className="content-modal-window">
+
+      </div>
+    </div>
+  )
+}
+
+export default TodoContentModal
